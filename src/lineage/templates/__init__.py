bbf43000b0f24fa257e0eca2272bd753dd65e623
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2025-05-21 14:04:43 -0400

    Prefer importlib to pkg_resources
    The latter is deprecated and is not included in the most recent
    version of types-setuptools.  This leads to an error from the mypy
    pre-commit hook.

---
+++ src/lineage/templates/__init__.py
@@ -0,0 +1,6 @@
+"""The lineage.templates package."""
+
+# This is necessary for the template_data line using importlib in the
+# load_template() function of entrypoint.py to correctly work with Python 3.9.
+# See here for more details:
+# https://setuptools.pypa.io/en/latest/userguide/datafiles.html?utm_source=chatgpt.com#namespace-support
